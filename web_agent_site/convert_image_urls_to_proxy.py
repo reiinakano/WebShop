157a8dd205e1758d73ff1ee902abdab96f13bc78
Reiichiro Nakano <reiichiro@openai.com>
2022-07-19 16:52:42 -0700

    .

--- web_agent_site/convert_image_urls_to_proxy.py
+++ web_agent_site/convert_image_urls_to_proxy.py
@@ -1,13 +1,15 @@
 import json
+from tqdm import tqdm
 
 from web_agent_site.utils import DEFAULT_FILE_PATH
 
 
 def main():
+    print(f"Opening {DEFAULT_FILE_PATH}")
     with open(DEFAULT_FILE_PATH, "r") as f:
         list_of_dicts = json.load(f)
 
-    for dct in list_of_dicts:
+    for dct in tqdm(list_of_dicts):
         for i in enumerate(len(dct["images"])):
             if dct["images"][i].startswith("https://m.media-amazon.com/"):
                 dct["images"][i] = dct["images"][i].replace("https://m.media-amazon.com/", "http://10.0.2.1:8050/p/")
