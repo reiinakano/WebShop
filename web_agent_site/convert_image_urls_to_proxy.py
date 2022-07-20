635415aa0d7dafbff7a7f879d1cfce3d0e6afa17
Reiichiro Nakano <reiichiro@openai.com>
2022-07-19 19:59:39 -0700

    missed options

--- web_agent_site/convert_image_urls_to_proxy.py
+++ web_agent_site/convert_image_urls_to_proxy.py
@@ -14,6 +14,16 @@ def main():
             if dct["images"][i].startswith("https://m.media-amazon.com/"):
                 dct["images"][i] = dct["images"][i].replace("https://m.media-amazon.com/", "http://10.0.2.1:8050/p/m.media-amazon.com/")
 
+        customization_options = dct["customization_options"]
+        if customization_options:
+            for _, option_contents in customization_options.items():
+                if option_contents is None:
+                    continue
+                for option_content in option_contents:
+                    option_image = option_content.get('image', None)
+                    if option_image and option_image.startswith("https://m.media-amazon.com/"):
+                        option_content["image"] = option_content["image"].replace("https://m.media-amazon.com/", "http://10.0.2.1:8050/p/m.media-amazon.com/")
+
     with open(DEFAULT_FILE_PATH, "w") as f:
         json.dump(list_of_dicts, f)
 
