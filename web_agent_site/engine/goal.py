ff8c7141bb4fbcdae192e77fdd128ee4726ae6eb
Reiichiro Nakano <reiichiro@openai.com>
2022-07-20 06:49:02 -0700

    .

--- web_agent_site/engine/goal.py
+++ web_agent_site/engine/goal.py
@@ -56,7 +56,7 @@ def get_human_goals(all_products, product_prices):
                 'attributes': attributes,
                 'price_upper': price_upper,
                 'goal_options': product['instruction_options'],
-                'uuid': generate_mturk_code(asin + product['instruction'].strip('.') + price_text),
+                'uuid': generate_mturk_code(asin + product['instruction'].strip('.') + price_text)[:8],
             })
             for att in attributes:
                 cnt_atts[att] += 1
